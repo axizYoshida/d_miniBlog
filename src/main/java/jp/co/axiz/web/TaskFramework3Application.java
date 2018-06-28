17e4193a6ab4eb2acd210db4b80e513d6652508b
AxiZ <AxiZ@WINDOWS-79AMK0H>
2018-06-29 08:45:30 +0900

    プロジェクトを作成しました

---
+++ src/main/java/jp/co/axiz/web/TaskFramework3Application.java
@@ -0,0 +1,12 @@
+package jp.co.axiz.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskFramework3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskFramework3Application.class, args);
+	}
+}
